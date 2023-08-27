772ac8b2f36388bbec24e1073972bb3ab602d782
@hexlet/cli <support@hexlet.io>
2023-08-27 23:09:08 +0400

    submit rails-basics-ru/static-pages

---
+++ rails-basics-ru/static-pages/config/routes.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # BEGIN
+
+  get 'pages/show'
+  get 'home/index'
+
+  # END
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
